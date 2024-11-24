86953df69279eabe746894fd2f19f953ed47fd35
Alex <alexrosenk@hotmail.se>
2024-11-24 17:33:27 +0100

    Added Engine.h

---
+++ include/Engine.h
@@ -0,0 +1,27 @@
+#ifndef ENGINE_H
+#define ENGINE_H
+#define SCREEN_WIDTH 1024
+#define SCREEN_HEIGHT 768
+#include <SDL2/SDL.h>
+namespace ichi {
+    class Engine {
+        public:
+        
+            // Singleton instance
+            static Engine* getInstance() { return instance = (instance != nullptr)? instance : new Engine(); }
+            SDL_Renderer* getRenderer() const { return renderer;};
+            void init();
+            void shutdown();
+            void quit();
+            void run();
+            bool isRunning() { return isRunning; }
+
+        private:
+            SDL_Window* window;
+            SDL_Renderer* renderer;
+            Engine(){}
+            bool isRunning = true;
+            static Engine* instance;
+    };
+}
+#endif
