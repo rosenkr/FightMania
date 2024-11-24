b386d25de4c2272d72fe3742b96754bdae6a2574
Alex <alexrosenk@hotmail.se>
2024-11-24 17:53:46 +0100

    Added partial Engine.cpp impl

---
+++ src/Engine.cpp
@@ -0,0 +1,24 @@
+#include "Engine.h"
+#include "TextureManager.h"
+
+namespace ichi {
+    Engine* Engine::instance = nullptr;
+
+    // TODO: add logging success/failure checks on renderer/window with spdlog, change to bool return type for use by run()?
+    void Engine::init() {
+        SDL_Init(SDL_INIT_EVERYTHING);
+        window = SDL_CreateWindow("Fighters", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+        renderer = SDL_CreateRenderer(window, -1, 0);
+        isRunning = true;
+    }
+
+    void Engine::shutdown() {
+        TextureManager::getInstance()->shutdown();
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+    }
+
+    // TODO: add run impl
+    void run() { }
+}
