803e034bb61e4d8a220c593429991b28a18a14eb
Alex <alexrosenk@hotmail.se>
2024-11-25 17:46:47 +0100

    Added header for animated sprite, with operator< impl. Used in TextureManager

--- include/Ichi/Core/animatedSprite.h
+++ include/Ichi/Core/animatedSprite.h
@@ -2,7 +2,14 @@
 #define ANIMATEDSPRITE_H
 namespace ichi::core {
     class AnimatedSprite{
-        
+        public: 
+        // Define operator< for Sprite comparison, usage in textureManager std::map
+        bool operator<(const AnimatedSprite& other) const {
+            return id < other.id;
+        }
+
+        private:
+        int id; // Temporary
     };
 }
 #endif
