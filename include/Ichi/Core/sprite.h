42b229020c51978b29dbf35f842d3e6857c970d9
Alex <alexrosenk@hotmail.se>
2024-11-25 17:48:00 +0100

    Added header for sprite, with operator< impl. Used in TextureManager

--- include/Ichi/Core/sprite.h
+++ include/Ichi/Core/sprite.h
@@ -2,7 +2,14 @@
 #define SPRITE_H
 namespace ichi::core {
     class Sprite{
+        public: 
+        // Define operator< for Sprite comparison, usage in textureManager std::map
+        bool operator<(const Sprite& other) const {
+            return id < other.id;
+        }
 
+        private:
+        int id; // Temporary
     };
 }
 #endif
