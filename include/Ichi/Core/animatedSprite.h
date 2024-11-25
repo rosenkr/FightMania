d80d6416d46704dd9dd3d51207ec12c591217a75
Alex <alexrosenk@hotmail.se>
2024-11-25 14:15:28 +0100

    Added headers to make textureManager compilable

---
+++ include/Ichi/Core/animatedSprite.h
@@ -0,0 +1,8 @@
+#ifndef ANIMATEDSPRITE_H
+#define ANIMATEDSPRITE_H
+namespace ichi::core {
+    class AnimatedSprite{
+        
+    };
+}
+#endif
---
+++ include/Ichi/Core/sprite.h
@@ -0,0 +1,8 @@
+#ifndef SPRITE_H
+#define SPRITE_H
+namespace ichi::core {
+    class Sprite{
+
+    };
+}
+#endif
