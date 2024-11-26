2b03d6485bb0d86dfa524210d63f5c03cd072901
maxi <maxi.ellnestam@gmail.com>
2024-11-26 16:55:02 +0100

    Small refactor and added toSdlRect

--- include/Ichi/Core/hitbox.h
+++ include/Ichi/Core/hitbox.h
@@ -3,19 +3,24 @@
 #include <SDL2/SDL.h>
 #include "point.h"
 
-namespace ichi::core {
-    class Hitbox {
-        public:
-            Hitbox(Point p, int w, int h){ rect.x = p.X; rect.y = p.Y; rect.w = w; rect.h = h;}
-            int getWidth() const { return rect.w; }
-            int getHeight() const { return rect.h; };
-            int getX() const { return rect.x; }
-            int getY() const { return rect.y; }
-            void setTangible(bool t) { isTangible = t; }
-            bool isOverlapping(const Hitbox& other) const;
-        private:
-            SDL_Rect rect;
-            bool isTangible = false;
+namespace ichi::core
+{
+    class Hitbox
+    {
+    public:
+        Hitbox(Point p, int w, int h, bool tangible) : rect(SDL_Rect{p.X, p.Y, w, h}), isTangible(tangible) {};
+
+        const SDL_Rect *toSdlRect() const { return &rect; }
+        int getWidth() const { return rect.w; }
+        int getHeight() const { return rect.h; }
+        int getX() const { return rect.x; }
+        int getY() const { return rect.y; }
+
+        bool isOverlapping(const Hitbox &other) const;
+
+    private:
+        SDL_Rect rect;
+        bool isTangible = false;
     };
 }
 #endif
