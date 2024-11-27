7e0ed5b79f81ec5398cc60c167800a819427bac9
Alex <alexrosenk@hotmail.se>
2024-11-27 09:58:28 +0100

    Revert "Add Sprite"
    This reverts commit 9b0531d02a72099551e9e059a181787f265fb58f.

--- include/Ichi/Graphics/sprite.h
+++ include/Ichi/Graphics/sprite.h
@@ -1,47 +1,18 @@
 #ifndef SPRITE_H
 #define SPRITE_H
-
-#include "Ichi/Core/hitbox.h"
-#include "Ichi/Graphics/textureManager.h"
-
-#include <string>
-#include <unordered_map>
-
 namespace ichi::graphics
 {
     class Sprite
     {
     public:
-        enum class Layer
+        // Define operator< for Sprite comparison, usage in textureManager std::map
+        bool operator<(const Sprite &other) const
         {
-            BACKGROUND,
-            MIDGROUND,
-            FOREGROUND,
-            DETAILS,
-            EFFECTS,
-            COLOR_ADJUSTMENT,
-            UICOMPONENT,
-        };
+            return id < other.id;
+        }
 
-        Layer getLayer() const { return layer; }
-        const core::Hitbox &getHitbox() const { return hitbox; }
-
-        void draw();
-        Sprite(core::Hitbox, Layer, std::string);
-        bool operator==(const Sprite &other) const;
-
-    protected:
-        Layer layer;
-        core::Hitbox hitbox;
-    };
-}
-
-namespace std
-{
-    template <>
-    struct hash<ichi::graphics::Sprite>
-    {
-        size_t operator()(const ichi::graphics::Sprite &sprite) const;
+    private:
+        int id; // Temporary
     };
 }
 #endif
