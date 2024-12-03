a7d650c0460f5b77c72580e50f5b3059062e5f7c
maxi <maxi.ellnestam@gmail.com>
2024-12-03 10:31:26 +0100

    Removed unused import

--- include/Ichi/Core/component.h
+++ include/Ichi/Core/component.h
@@ -1,23 +1,27 @@
 #ifndef COMPONENT_H
 #define COMPONENT_H
-#include <SDL2/SDL.h>
-#include "hitbox.h"
-namespace ichi::core {
-    class Component {
-        public:
-            virtual void update() = 0;
-            virtual void draw() const = 0;
-            virtual void handleinput() {}; 
-            virtual ~Component() = default;
 
-            const Hitbox& getHitbox() const{ return hitbox; } // Getter in case we want to draw textures based on hitbox size
+#include "Ichi/Core/hitbox.h"
 
-            // Prefer newer idiom of public + delete over making these private
-            Component(const Component&) = delete;
-            Component& operator=(const Component&) = delete;
-        protected:
-            Hitbox hitbox;
-            Component(Hitbox hb): hitbox(hb){};
+namespace ichi::core
+{
+    class Component
+    {
+    public:
+        virtual void update() = 0;
+        virtual void draw() const = 0;
+        virtual void handleinput() {};
+        virtual ~Component() = default;
+
+        const Hitbox &getHitbox() const { return hitbox; } // Getter in case we want to draw textures based on hitbox size
+
+        // Prefer newer idiom of public + delete over making these private
+        Component(const Component &) = delete;
+        Component &operator=(const Component &) = delete;
+
+    protected:
+        Hitbox hitbox;
+        Component(Hitbox hb) : hitbox(hb) {};
     };
 }
 #endif
