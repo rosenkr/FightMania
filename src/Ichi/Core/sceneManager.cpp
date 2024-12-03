816b976794df422fac241f7417dbe26dbe4f0ed9
maxi <maxi.ellnestam@gmail.com>
2024-12-03 10:31:43 +0100

    Small refactor

--- src/Ichi/Core/sceneManager.cpp
+++ src/Ichi/Core/sceneManager.cpp
@@ -4,45 +4,53 @@
 
 // Currently just a namespace library, not a class
 // Uses a vector but perhaps another data structure is more reasonable
-namespace ichi::core::sceneManager {
+namespace ichi::core::sceneManager
+{
 
     // Shared instead of unique because activeScene wants to point at a scene pointed to in scenes
-    static std::vector<std::shared_ptr<Scene>> scenes; 
+    static std::vector<std::shared_ptr<Scene>> scenes;
     static std::shared_ptr<Scene> activeScene;
 
     // adds existing scene at the given index
     // The implementation must create a new scene and make it uniquely pointed to, then pass ownership to the manager by mgr.addScene, which calls move().
-    void addScene(std::shared_ptr<Scene> scene, size_t index) {
-        if (scenes.size() <= index) { 
+    void addScene(std::shared_ptr<Scene> scene, size_t index)
+    {
+        if (scenes.size() <= index)
             scenes.resize(index + 1);
-        } 
-        scenes.insert(scenes.begin()+index, scene);
+
+        scenes.insert(scenes.begin() + index, scene);
     }
 
     // If index is within the proper range and scenes isnt empty, remove scene at index
-    void removeScene(size_t index) {
-        if(index >= 0 && index < scenes.size()) {
-            scenes.erase(scenes.begin()+index);
-        } else {
+    void removeScene(size_t index)
+    {
+        if (index < 0 || index >= scenes.size())
+        {
             ICHI_ERROR("Index error when removing scene at index");
+            return;
         }
+
+        scenes.erase(scenes.begin() + index);
     }
 
     // If index in valid range and there is a unique pointer to a scene at the index, set that scene as active one.
     // Before setting the new scene, transfer back the old scene to the components vector
-    void setActiveScene(size_t index) {
-        if(index >= 0 && index < scenes.size() && scenes.at(index) != nullptr) {
-            activeScene = scenes.at(index); 
-        } else {
+    void setActiveScene(size_t index)
+    {
+        if (index < 0 || index >= scenes.size() || scenes.at(index) == nullptr)
+        {
             ICHI_ERROR("Index error when setting active scene to scene at index");
+            return;
         }
+
+        activeScene = scenes.at(index);
     }
 
-    // call update on (active scene OR all scenes?), which calls update on its components
-    // because what if a scene is updated even when it isnt active? 
-    //void update();
+    // call update on active scene, which calls update on its components
+    // because what if a scene is updated even when it isnt active?
+    // void update();
 
     // call draw on the active scene
-    //void draw();
+    // void draw();
 
 }
