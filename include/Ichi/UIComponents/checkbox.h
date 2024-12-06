061bca41d29c51dd3988921543d98ea9a2aa45b4
Alex <alexrosenk@hotmail.se>
2024-12-06 14:30:15 +0100

    Added Checkbox

---
+++ include/Ichi/UIComponents/checkbox.h
@@ -0,0 +1,19 @@
+#ifndef CHECKBOX_H
+#define CHECKBOX_H
+#include "Ichi/UIComponents/uicomponent.h"
+#include "Ichi/Graphics/sprite.h"
+#include "Ichi/DataTypes/hitbox.h"
+namespace ichi::uicomponents{
+    class Checkbox : UIComponent {
+        public: 
+            Checkbox(const datatypes::Hitbox& hb, const graphics::Sprite& us, const graphics::Sprite& cs, bool checked = false);
+            void update();
+            void draw() const;
+        private:
+            bool isChecked;
+            graphics::Sprite uncheckedSprite;
+            graphics::Sprite checkedSprite;
+    };
+}
+
+#endif
