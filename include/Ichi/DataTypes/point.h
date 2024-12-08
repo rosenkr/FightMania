7941335d67139eaa82aae57073e4201382417def
maxi <maxi.ellnestam@gmail.com>
2024-12-08 19:52:08 +0100

    Add functionallity to Point

--- include/Ichi/DataTypes/point.h
+++ include/Ichi/DataTypes/point.h
@@ -1,6 +1,8 @@
 #ifndef POINT_H
 #define POINT_H
 
+#include <cmath>
+
 namespace ichi::datatypes
 {
     class Point
@@ -8,6 +10,23 @@ namespace ichi::datatypes
     public:
         Point(int x = 0, int y = 0) : X(x), Y(y) {}
         int X, Y;
+
+        float distanceTo(const Point &other) const
+        {
+            int dx = other.X - X;
+            int dy = other.Y - Y;
+
+            return std::sqrt(dx * dx + dy * dy);
+        }
+
+        bool operator==(const Point &other) const { return other.X == X && other.Y == Y; }
+        bool operator!=(const Point &other) const { return !(other == *this); }
+        bool operator<(const Point &other) const
+        {
+            if (this->Y != other.Y)
+                return this->Y < other.Y;
+            return this->X < other.X;
+        }
 
         /* Overloaded operators */
     };
