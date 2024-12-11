01577c346aa634e2fe46df6f789f5e9b6e46cf71
Alex <alexrosenk@hotmail.se>
2024-12-11 18:54:16 +0100

    fightmain

---
+++ src/Ichi/DataTypes/Core/window.cpp
@@ -0,0 +1,66 @@
+#include "Ichi/Core/window.h"
+
+#include "Ichi/Core/engine.h"
+#include "Ichi/Input/controllerHandler.h"
+#include "Ichi/Input/mouse.h"
+#include "Ichi/Input/keyboard.h"
+#include "Ichi/Scene/sceneManager.h"
+
+namespace ichi::core
+{
+
+    Window::Window()
+    {
+        SDL_CreateWindowAndRenderer(WINDOW_WIDTH, WINDOW_HEIGHT, 0, &window, &renderer);
+    }
+
+    Window::~Window()
+    {
+        SDL_DestroyWindow(window);
+        window = nullptr;
+    }
+
+    void Window::handleEvents()
+    {
+        SDL_Event e;
+        while (SDL_PollEvent(&e))
+        {
+            switch (e.type)
+            {
+            case SDL_QUIT:
+                Engine::getInstance()->quit();
+                break;
+
+            case SDL_CONTROLLERDEVICEADDED:
+                input::ControllerHandler::onControllerConnect(e.cdevice);
+                break;
+
+            case SDL_CONTROLLERDEVICEREMOVED:
+                input::ControllerHandler::onControllerDisconnect(e.cdevice);
+                break;
+            default:
+                break;
+            }
+        }
+        // update input handlers
+
+        input::Mouse::update();
+        input::Keyboard::update();
+        input::ControllerHandler::update();
+    }
+
+    void Window::draw()
+    {
+        SDL_RenderClear(Engine::getInstance()->getRenderer());
+
+        scene::sceneManager::draw();
+
+        SDL_RenderPresent(Engine::getInstance()->getRenderer());
+    }
+
+    void Window::update()
+    {
+        scene::sceneManager::update();
+    }
+
+} // namespace ichi::core
--- src/Implementation/fightMania.cpp
+++ src/Implementation/fightMania.cpp
@@ -27,8 +27,8 @@ int main(int argc, char *argv[])
 	}
 
 	// Temp string for Alex
-	// std::string str = "/home/sasha/Documents/Uni/Course3/projects/fight_mania/";
-	std::string str = "";
+	 std::string str = "/home/sasha/Documents/Uni/Course3/projects/fight_mania/";
+	//std::string str = "";
 	TTF_Font *font = TTF_OpenFont((str + "resources/fonts/PRSTART.TTF").c_str(), 20);
 
 	datatypes::Hitbox window(datatypes::Point(0, 0), 384 * 4, 224 * 4, false);
@@ -38,18 +38,18 @@ int main(int argc, char *argv[])
 	datatypes::Hitbox hb2(datatypes::Point(100, 400), 50, 50, false);
 	datatypes::Hitbox hb3(datatypes::Point(200, 400), 50, 50, false);
 
-	datatypes::Hitbox hb4(datatypes::Point(500, 500), 100, 20, false);
+	datatypes::Hitbox hb4(datatypes::Point(500, 500), 150, 35, false);
 
-	graphics::Sprite bar(hb4, graphics::Sprite::Layer::UICOMPONENT, "resources/images/UIComponents/Bar.png");
+	graphics::Sprite bar(hb4, graphics::Sprite::Layer::UICOMPONENT, str + "resources/images/UIComponents/Bar.png");
 
-	graphics::Sprite s(hb, graphics::Sprite::Layer::UICOMPONENT, "resources/images/UIComponents/TextBox.png");
-	graphics::Sprite s1(hb1, graphics::Sprite::Layer::UICOMPONENT, "resources/images/UIComponents/TextBox.png");
-	graphics::Sprite s2(hb2, graphics::Sprite::Layer::UICOMPONENT, "resources/images/UIComponents/TextBox.png");
-	graphics::Sprite s3(hb3, graphics::Sprite::Layer::UICOMPONENT, "resources/images/UIComponents/TextBox.png");
-	graphics::Sprite sChecked(hb, graphics::Sprite::Layer::UICOMPONENT, "resources/images/UIComponents/FocusedTextbox.png");
-	graphics::Sprite sChecked1(hb1, graphics::Sprite::Layer::UICOMPONENT, "resources/images/UIComponents/FocusedTextbox.png");
-	graphics::Sprite sChecked2(hb2, graphics::Sprite::Layer::UICOMPONENT, "resources/images/UIComponents/FocusedTextbox.png");
-	graphics::Sprite sChecked3(hb3, graphics::Sprite::Layer::UICOMPONENT, "resources/images/UIComponents/FocusedTextbox.png");
+	graphics::Sprite s(hb, graphics::Sprite::Layer::UICOMPONENT, str +"resources/images/UIComponents/TextBox.png");
+	graphics::Sprite s1(hb1, graphics::Sprite::Layer::UICOMPONENT, str +"resources/images/UIComponents/TextBox.png");
+	graphics::Sprite s2(hb2, graphics::Sprite::Layer::UICOMPONENT, str +"resources/images/UIComponents/TextBox.png");
+	graphics::Sprite s3(hb3, graphics::Sprite::Layer::UICOMPONENT, str +"resources/images/UIComponents/TextBox.png");
+	graphics::Sprite sChecked(hb, graphics::Sprite::Layer::UICOMPONENT, str +"resources/images/UIComponents/FocusedTextbox.png");
+	graphics::Sprite sChecked1(hb1, graphics::Sprite::Layer::UICOMPONENT, str +"resources/images/UIComponents/FocusedTextbox.png");
+	graphics::Sprite sChecked2(hb2, graphics::Sprite::Layer::UICOMPONENT, str +"resources/images/UIComponents/FocusedTextbox.png");
+	graphics::Sprite sChecked3(hb3, graphics::Sprite::Layer::UICOMPONENT, str +"resources/images/UIComponents/FocusedTextbox.png");
 
 	std::shared_ptr<uicomponents::UIComponent> c = std::make_shared<uicomponents::Textbox>(hb, font, SDL_Color{0, 255, 0, 255}, s, sChecked);
 	std::shared_ptr<uicomponents::UIComponent> c1 = std::make_shared<uicomponents::Textbox>(hb1, font, SDL_Color{0, 255, 0, 255}, s1, sChecked1);
@@ -57,7 +57,7 @@ int main(int argc, char *argv[])
 	std::shared_ptr<uicomponents::UIComponent> c3 = std::make_shared<uicomponents::Textbox>(hb3, font, SDL_Color{0, 255, 0, 255}, s3, sChecked3);
 
 	// uicomponents::Pane *pane = new uicomponents::Pane(window, {c, c1, c2, c3});
-	uicomponents::UIComponent *uic = new uicomponents::SlideBar(hb4, bar, 10, 30, "resources/images/UIComponents/Slider.png", "resources/images/UIComponents/FocusedSlider.png");
+	uicomponents::UIComponent *uic = new uicomponents::SlideBar(hb4, bar, 10, 30, str +"resources/images/UIComponents/Slider.png", str + "resources/images/UIComponents/FocusedSlider.png");
 
 	std::shared_ptr<scene::Menu> scene = std::make_shared<scene::Menu>(std::vector<core::Component *>{}, std::vector<uicomponents::UIComponent *>{uic});
 
