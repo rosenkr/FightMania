82b60b713c0fbb6b45fbd18d51c1b71ea9978550
maxi <maxi.ellnestam@gmail.com>
2024-12-17 12:30:15 +0100

    Add check for if the animation has looped one time

--- src/Ichi/Graphics/animatedSprite.cpp
+++ src/Ichi/Graphics/animatedSprite.cpp
@@ -25,7 +25,10 @@ namespace ichi::graphics
             return;
 
         if (++currentFrame > (int)frameTime.size() - 1)
+        {
             currentFrame = 0;
+            compleatedALap = true;
+        }
 
         lastUpdatedOn = SDL_GetTicks();
     }
