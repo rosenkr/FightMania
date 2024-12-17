3170deabba43fdf1d223bab46c09348b36046310
maxi <maxi.ellnestam@gmail.com>
2024-12-17 15:07:46 +0100

    Add CutSceneHandler

---
+++ src/Implementation/cutsceneHandler.cpp
@@ -0,0 +1,31 @@
+#include "Implementation/cutsceneHandler.h"
+
+std::queue<std::pair<ichi::graphics::AnimatedSprite, Mix_Chunk *>> CutsceneHandler::queue = {};
+
+void CutsceneHandler::update()
+{
+    if (queue.front().first.hasCompleatedALap())
+    {
+        queue.pop();
+        return;
+    }
+
+    queue.front().first.update();
+
+    if (!queue.front().second)
+        return;
+
+    ichi::audio::AudioPlayer::play(queue.front().second);
+    queue.front().second = nullptr;
+}
+
+void CutsceneHandler::draw()
+{
+    auto as = queue.front().first;
+    as.draw();
+}
+
+void CutsceneHandler::addCutscene(ichi::graphics::AnimatedSprite as, Mix_Chunk *chunk)
+{
+    queue.push(std::pair<ichi::graphics::AnimatedSprite, Mix_Chunk *>(as, chunk));
+}
