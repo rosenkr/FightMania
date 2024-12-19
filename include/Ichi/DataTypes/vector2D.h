56bfe0a837ea43e6647a6779fa71b66399240cd5
maxi <maxi.ellnestam@gmail.com>
2024-12-19 13:31:39 +0100

    Add some overloaded operators

--- include/Ichi/DataTypes/vector2D.h
+++ include/Ichi/DataTypes/vector2D.h
@@ -1,7 +1,7 @@
 #ifndef VECTOR2D_H
 #define VECTOR2D_H
 #include <cmath>
-namespace ichi
+namespace ichi::datatypes
 {
     class Vector2D
     {
@@ -11,7 +11,8 @@ namespace ichi
         float getX() const { return X; }
         float getY() const { return Y; }
 
-        void setY(float y) { Y = y;}
+        void setY(float y) { Y = y; }
+        void setX(float x) { X = x; }
 
         float magnitude() const { return std::sqrt(X * X + Y * Y); }
         Vector2D normalize() const
@@ -20,11 +21,36 @@ namespace ichi
             return (mag != 0) ? Vector2D(X / mag, Y / mag) : Vector2D(0, 0);
         }
 
-        Vector2D operator+(const Vector2D &v2) const { return Vector2D(X + v2.X, Y + v2.Y); }
-        Vector2D operator-(const Vector2D &v2) const { return Vector2D(X - v2.X, Y - v2.Y); }
+        Vector2D &operator+=(const Vector2D &rhs)
+        {
+            X += rhs.X;
+            Y += rhs.Y;
+            return *this; // return the result by reference
+        }
+
+        friend Vector2D operator+(Vector2D lhs, const Vector2D &rhs)
+        {
+            lhs += rhs;
+            return lhs;
+        }
+
+        Vector2D &operator-=(const Vector2D &rhs)
+        {
+            X -= rhs.X;
+            Y -= rhs.Y;
+            return *this; // return the result by reference
+        }
+
+        friend Vector2D operator-(Vector2D lhs, const Vector2D &rhs)
+        {
+            lhs -= rhs;
+            return lhs;
+        }
+
         Vector2D operator/(float scaler) const { return (scaler != 0) ? Vector2D(X / scaler, Y / scaler) : Vector2D(0, 0); }
         Vector2D operator*(float scaler) const { return Vector2D(X * scaler, Y * scaler); } // Scalar multiplication
-        float operator*(const Vector2D &v2) const { return X * v2.X + Y * v2.Y; }           // Dot product
+
+        float operator*(const Vector2D &v2) const { return X * v2.X + Y * v2.Y; } // Dot product
     private:
         float X, Y;
     };
