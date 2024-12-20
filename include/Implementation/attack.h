2417e77c3d05260a5df5f3278c4a9b501b694ddc
Alex <alexrosenk@hotmail.se>
2024-12-20 03:04:37 +0100

    projectile attack

--- include/Implementation/attack.h
+++ include/Implementation/attack.h
@@ -5,15 +5,21 @@
 #include "Ichi/DataTypes/vector2D.h"
 #include "Ichi/DataTypes/hitbox.h"
 #include "Ichi/Graphics/animatedSprite.h"
+#include "Ichi/log.h"
 #include <memory>
 
-class Attack : public ichi::core::Component{
+class Attack : public ichi::core::Component, public std::enable_shared_from_this<Attack>{
     public:
-        Attack(ichi::datatypes::Hitbox hb, std::shared_ptr<ichi::graphics::AnimatedSprite> animation): Component(hb), animation(animation){}
+        Attack(ichi::datatypes::Hitbox hb, std::shared_ptr<ichi::graphics::AnimatedSprite> animation, ichi::datatypes::Vector2D direction, float dmg, float speed): Component(hb), animation(std::move(animation)), direction(direction), dmg(dmg), speed(speed) {}
+        ichi::datatypes::Vector2D getDirection() const { return direction; }
         void update();
         void draw() const;
+        void moveInDirectionWithSpeed();
     private:
         std::shared_ptr<ichi::graphics::AnimatedSprite> animation;
+        ichi::datatypes::Vector2D direction; 
+        float dmg;
+        float speed;
 
 };
 
@@ -21,16 +27,12 @@ class Attack : public ichi::core::Component{
 // cant have animation because animations need hitbox which is only relevant for actual attacks on the screen
 class AttackType{
     public:
-        AttackType(std::vector<std::string> paths, float dmg, float speed, int width, int height): paths(paths), dmg(dmg), speed(speed), width(width), height(height) {}
+        AttackType(std::vector<std::string> paths, int width, int height): paths(paths), width(width), height(height) {}
         std::vector<std::string> getPaths() const { return paths; }
-        float getDmg() const { return dmg; }
-        float getSpeed() const { return speed; }
         int getWidth() const { return width; }
         int getHeight() const { return height; }
     private:
         std::vector<std::string> paths;
-        float dmg;
-        float speed;
         int width;
         int height;
 };
