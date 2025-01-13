8607896cd9638ce48bc430b8817ee76579912e89
maxi <maxi.ellnestam@gmail.com>
2025-01-13 11:51:14 +0100

    Add cooldown to attack

--- include/Implementation/attack.h
+++ include/Implementation/attack.h
@@ -9,17 +9,20 @@ class Character; // Forward delcalration
 class Attack
 {
 public:
-    virtual void draw() const = 0;
-    virtual void update() = 0;
-    virtual void reset(ichi::datatypes::Point) = 0;
+    virtual void draw(bool) const = 0;
+    virtual void update(ichi::datatypes::Point, bool) = 0;
+    virtual void reset() = 0;
     virtual bool hits(Character c) = 0;
     virtual bool isDone() = 0;
+    bool canAttack() { return SDL_GetTicks() > lastUsed + cooldownTime; }
 
 protected:
-    Attack(float dmg) : damage(dmg) {}
+    Attack(float dmg, Uint32 cooldown) : damage(dmg), cooldownTime(cooldown) {}
     ~Attack() = default;
 
     float damage;
+    Uint32 cooldownTime;
+    Uint32 lastUsed = 0;
 };
 
 #endif // ATTACK_H
