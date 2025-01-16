6af78d44391e72e413508c6c58a1134c68a0e7d6
maxi <maxi.ellnestam@gmail.com>
2025-01-16 23:18:35 +0100

    Add sfx

--- include/Implementation/match.h
+++ include/Implementation/match.h
@@ -41,25 +41,44 @@ private:
         return std::unique_ptr<Mix_Chunk, MixChunkDeleter>(chunk);
     }
 
-    const std::string SOUND_EFFECT_START_PATH = constants::gResPath + "sounds/ChestOpening.wav";
-    const std::string SOUND_EFFECT_KO_PATH = constants::gResPath + "/sounds/ChestOpening.wav";
-    const std::string SOUND_EFFECT_PERFECT_PATH = constants::gResPath + "sounds/ChestOpening.wav";
-    const std::string SOUND_EFFECT_BLUE_WINS_PATH = constants::gResPath + "sounds/ChestOpening.wav";
-    const std::string SOUND_EFFECT_RED_WINS_PATH = constants::gResPath + "sounds/ChestOpening.wav";
+    const std::string SOUND_EFFECT_FIGHT_PATH = constants::gResPath + "sounds/fight.wav";
+    const std::string SOUND_EFFECT_KO_PATH = constants::gResPath + "sounds/KO.wav";
+    const std::string SOUND_EFFECT_PERFECT_PATH = constants::gResPath + "sounds/perfect.wav";
+    const std::string SOUND_EFFECT_BLUE_WINS_PATH = constants::gResPath + "sounds/blueWins.wav";
+    const std::string SOUND_EFFECT_RED_WINS_PATH = constants::gResPath + "sounds/redWins.wav";
 
-    std::unique_ptr<Mix_Chunk, MixChunkDeleter> startSf;
+    const std::string SOUND_EFFECT_ROUND_1_PATH = constants::gResPath + "sounds/roundOne.wav";
+    const std::string SOUND_EFFECT_ROUND_2_PATH = constants::gResPath + "sounds/roundTwo.wav";
+    const std::string SOUND_EFFECT_ROUND_3_PATH = constants::gResPath + "sounds/roundThree.wav";
+    const std::string SOUND_EFFECT_ROUND_4_PATH = constants::gResPath + "sounds/roundFour.wav";
+    const std::string SOUND_EFFECT_ROUND_5_PATH = constants::gResPath + "sounds/roundFive.wav";
+
+    std::unique_ptr<Mix_Chunk, MixChunkDeleter> fightSf;
+    std::unique_ptr<Mix_Chunk, MixChunkDeleter> round1Sf;
+    std::unique_ptr<Mix_Chunk, MixChunkDeleter> round2Sf;
+    std::unique_ptr<Mix_Chunk, MixChunkDeleter> round3Sf;
+    std::unique_ptr<Mix_Chunk, MixChunkDeleter> round4Sf;
+    std::unique_ptr<Mix_Chunk, MixChunkDeleter> round5Sf;
     std::unique_ptr<Mix_Chunk, MixChunkDeleter> KOSf;
     std::unique_ptr<Mix_Chunk, MixChunkDeleter> perfectSf;
     std::unique_ptr<Mix_Chunk, MixChunkDeleter> blueWinsSf;
     std::unique_ptr<Mix_Chunk, MixChunkDeleter> redWinsSf;
 
-    const std::string START_PATH = constants::gResPath + "images/CutScenes/StartCutscene";
+    const std::string FIGHT_PATH = constants::gResPath + "images/CutScenes/Fight.png";
+
+    const std::string ROUND_1_PATH = constants::gResPath + "images/CutScenes/RoundOne.png";
+    const std::string ROUND_2_PATH = constants::gResPath + "images/CutScenes/RoundTwo.png";
+    const std::string ROUND_3_PATH = constants::gResPath + "images/CutScenes/RoundThree.png";
+    const std::string ROUND_4_PATH = constants::gResPath + "images/CutScenes/RoundFour.png";
+    const std::string ROUND_5_PATH = constants::gResPath + "images/CutScenes/RoundFive.png";
+
     const std::string KO_PATH = constants::gResPath + "images/CutScenes/KO.png";
     const std::string PERFECT_PATH = constants::gResPath + "images/CutScenes/Perfect.png";
     const std::string BLUE_WINS_PATH = constants::gResPath + "images/CutScenes/BlueWins.png";
     const std::string RED_WINS_PATH = constants::gResPath + "images/CutScenes/RedWins.png";
 
-    std::map<int, Uint32> startTime = {{0, 1000}, {1, 1000}, {2, 1000}, {3, 500}};
+    std::map<int, Uint32> fightTime = {{0, 2000}};
+    std::map<int, Uint32> roundTime = {{0, 2500}};
     std::map<int, Uint32> KOTime = {{0, 3000}};
     std::map<int, Uint32> perfectTime = {{0, 2000}};
     std::map<int, Uint32> blueWinsTime = {{0, 4000}};
@@ -73,10 +92,17 @@ private:
     bool suddenDeathActive = false;
     bool end = false;
 
+    int round = 1;
+
     std::shared_ptr<Character> blueCharacter;
     std::shared_ptr<Character> redCharacter;
 
-    ichi::graphics::AnimatedSprite start;
+    ichi::graphics::AnimatedSprite fight;
+    ichi::graphics::AnimatedSprite round1;
+    ichi::graphics::AnimatedSprite round2;
+    ichi::graphics::AnimatedSprite round3;
+    ichi::graphics::AnimatedSprite round4;
+    ichi::graphics::AnimatedSprite round5;
     ichi::graphics::AnimatedSprite KO;
     ichi::graphics::AnimatedSprite perfect;
     ichi::graphics::AnimatedSprite blueWins;
--- src/Implementation/match.cpp
+++ src/Implementation/match.cpp
@@ -5,14 +5,24 @@
 
 Match::Match(std::shared_ptr<Character> blue, std::shared_ptr<Character> red, TTF_Font *font)
     : core::Component(datatypes::Hitbox(datatypes::Point(0, 0), 384, 224, false)), blueCharacter(std::move(blue)), redCharacter(std::move(red)),
-      start(ichi::graphics::AnimatedSprite(hitbox, graphics::Sprite::Layer::FOREGROUND, START_PATH, startTime.size(), startTime)),
+      fight(ichi::graphics::AnimatedSprite(hitbox, graphics::Sprite::Layer::FOREGROUND, {FIGHT_PATH}, fightTime)),
+      round1(ichi::graphics::AnimatedSprite(hitbox, graphics::Sprite::Layer::FOREGROUND, {ROUND_1_PATH}, roundTime)),
+      round2(ichi::graphics::AnimatedSprite(hitbox, graphics::Sprite::Layer::FOREGROUND, {ROUND_2_PATH}, roundTime)),
+      round3(ichi::graphics::AnimatedSprite(hitbox, graphics::Sprite::Layer::FOREGROUND, {ROUND_3_PATH}, roundTime)),
+      round4(ichi::graphics::AnimatedSprite(hitbox, graphics::Sprite::Layer::FOREGROUND, {ROUND_4_PATH}, roundTime)),
+      round5(ichi::graphics::AnimatedSprite(hitbox, graphics::Sprite::Layer::FOREGROUND, {ROUND_5_PATH}, roundTime)),
       KO(ichi::graphics::AnimatedSprite(hitbox, graphics::Sprite::Layer::FOREGROUND, {KO_PATH}, KOTime)),
       perfect(ichi::graphics::AnimatedSprite(hitbox, graphics::Sprite::Layer::FOREGROUND, {PERFECT_PATH}, perfectTime)),
       blueWins(ichi::graphics::AnimatedSprite(hitbox, graphics::Sprite::Layer::FOREGROUND, {BLUE_WINS_PATH}, blueWinsTime)),
       redWins(ichi::graphics::AnimatedSprite(hitbox, graphics::Sprite::Layer::FOREGROUND, {RED_WINS_PATH}, redWinsTime))
 {
 
-    startSf = loadSoundEffect(SOUND_EFFECT_START_PATH);
+    fightSf = loadSoundEffect(SOUND_EFFECT_FIGHT_PATH);
+    round1Sf = loadSoundEffect(SOUND_EFFECT_ROUND_1_PATH);
+    round2Sf = loadSoundEffect(SOUND_EFFECT_ROUND_2_PATH);
+    round3Sf = loadSoundEffect(SOUND_EFFECT_ROUND_3_PATH);
+    round4Sf = loadSoundEffect(SOUND_EFFECT_ROUND_4_PATH);
+    round5Sf = loadSoundEffect(SOUND_EFFECT_ROUND_5_PATH);
     KOSf = loadSoundEffect(SOUND_EFFECT_KO_PATH);
     perfectSf = loadSoundEffect(SOUND_EFFECT_PERFECT_PATH);
     blueWinsSf = loadSoundEffect(SOUND_EFFECT_BLUE_WINS_PATH);
@@ -82,6 +92,7 @@ void Match::update()
 
     if (blueCharacter.get()->getHp() <= 0)
     {
+        round++;
         CutsceneHandler::addCutscene(KO, KOSf.get());
 
         if (redCharacter.get()->getHp() == Character::MAX_HP)
@@ -99,7 +110,7 @@ void Match::update()
 
     if (redCharacter.get()->getHp() <= 0)
     {
-
+        round++;
         CutsceneHandler::addCutscene(KO, KOSf.get());
 
         if (blueCharacter.get()->getHp() == Character::MAX_HP)
@@ -125,11 +136,12 @@ void Match::startSuddenDeath()
     redCharacter.get()->setPosition(datatypes::Point(300, 50));
     blueCharacter.get()->takeDamage(blueCharacter.get()->getHp() - 1, datatypes::Vector2D(0, 0)); // sets the hp to 1
     redCharacter.get()->takeDamage(redCharacter.get()->getHp() - 1, datatypes::Vector2D(0, 0));
-    CutsceneHandler::addCutscene(start, startSf.get());
+    CutsceneHandler::addCutscene(fight, fightSf.get());
 }
 
 void Match::startSet()
 {
+    round = 1;
     matchTime = 0;
     timeLastUpdated = 0;
     suddenDeathActive = false;
@@ -137,5 +149,26 @@ void Match::startSet()
     redCharacter.get()->setPosition(datatypes::Point(300, 50));
     blueCharacter.get()->reset();
     redCharacter.get()->reset();
-    CutsceneHandler::addCutscene(start, startSf.get());
+    switch (round)
+    {
+    case 1:
+        CutsceneHandler::addCutscene(round1, round1Sf.get());
+        break;
+    case 2:
+        CutsceneHandler::addCutscene(round2, round2Sf.get());
+        break;
+    case 3:
+        CutsceneHandler::addCutscene(round3, round3Sf.get());
+        break;
+    case 4:
+        CutsceneHandler::addCutscene(round4, round4Sf.get());
+        break;
+    case 5:
+        CutsceneHandler::addCutscene(round5, round5Sf.get());
+        break;
+
+    default:
+        break;
+    }
+    CutsceneHandler::addCutscene(fight, fightSf.get());
 }
